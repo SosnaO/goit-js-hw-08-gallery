ba9015401bd30071186df549131d9e26efda4878
SosnaO <solivijasosna@gmail.com>
2021-04-17 20:39:48 +0300

    6

--- src/task8.js
+++ src/task8.js
@@ -3,12 +3,9 @@ const gallery = document.querySelector('.js-gallery');
 const openModalBtn = document.querySelector('.js-lightbox');
 const lightboxImageEl = openModalBtn.querySelector('.lightbox__image');
 const closeModalBtn = document.querySelector('[data-action="close-lightbox"]')
-
 const lightboxOverlay = openModalBtn.querySelector('.lightbox__overlay');
+
 lightboxOverlay.addEventListener('click', onLightboxOverlay);
-
-const imagesSrc = [];
-
 gallery.addEventListener('click', onOpenModal);
 closeModalBtn.addEventListener('click', onCloseModal);
 
@@ -33,26 +30,21 @@ function createGalleryItemMarkup(images) {
 </li>
       `;
   }).join('');
- 
-}
-
+ }
 function onOpenModal(event) {
   event.preventDefault('');
   openModalBtn.classList.add('is-open');
   lightboxImageEl.src = event.target.dataset.source;
   window.addEventListener('keydown', onEscKeydown);
 }
-
 function onCloseModal() {
   openModalBtn.classList.remove('is-open');
   lightboxImageEl.src = '';
   window.removeEventListener('keydown',onEscKeydown)
-
  }
 function onLightboxOverlay() {
   onCloseModal();
 }
-
 function onEscKeydown(event) {
   if (event.code === 'Escape'){
     onCloseModal();
